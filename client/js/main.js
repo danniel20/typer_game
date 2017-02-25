a0e2cd62e261203fc36809e5d77251457c2e70d5
Danniel Albuquerque <danniel.master@gmail.com>
2017-02-25 12:48:46 -0300

    teste adicionando eventos

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
     $('.tooltip').tooltipster();
+
+    atualizaTamanhoFrase();
+    inicializaCronometro();
+    reiniciarJogo();
 });
+
+var $campo = $(".campo-digitacao");
+var $tempoInicial = $(".tempo");
+
+function atualizaTamanhoFrase(){
+	var palavras_na_frase = $(".frase").text().split(" ").length;
+	$(".numero-palavras").text(palavras_na_frase);
+}
+
+function inicializaCronometro(){
+	$campo.one("focus", function(){
+
+		var cronometroID = setInterval(function(){
+			var tempo_restante = $tempoInicial.text();
+			tempo_restante--;
+			$tempoInicial.text(tempo_restante);
+			
+			if(tempo_restante < 1){
+				console.log("acabou");
+				clearInterval(cronometroID);
+
+				$campo.attr("disabled", true);
+				$campo.toggleClass("campo-desativado");
+			}
+
+		}, 1000);
+	});
+	
+}
+
+function reiniciarJogo(){
+	$(".botao-reiniciar").on("click", function(){
+		$campo.attr("disabled", false);
+		$campo.toggleClass("campo-desativado");
+		$tempoInicial.text(5);
+
+		inicializaCronometro();
+	});
+}
+
+
